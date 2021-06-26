87f8aff81d7f45f36fbaf961a93ca55de82f0e6e
Lewis Murray <lewisdmurray00@gmail.com>
2021-06-26 16:06:22 +0100

    Added a title and home page html section

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
+//toggles the hamburger menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
